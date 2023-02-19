3b50ff3f2ce9bb2e534aa4b0a82acdbeeed078ad
jaehyun <wogustkfkd11@gmail.com>
2023-02-19 21:05:22 +0900

    ResponseStatusExceptionResolver의 @ResponseStatus

--- src/main/java/hello/exception/exception/BadRequestException.java
+++ src/main/java/hello/exception/exception/BadRequestException.java
@@ -3,7 +3,11 @@ package hello.exception.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-//코드 수정 불가능한 라이브러리에 적용 불가 -> ResponseStatusException 쓰기
+/**
+ * ResponseStatusExceptionResolver
+ *
+ * @ResponseStatus: 예외를 HTTP리 상태 코드로 예외를 변경해줌!
+ */
 @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "error.bad")
 public class BadRequestException extends RuntimeException {
 
