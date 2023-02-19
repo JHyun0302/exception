2c8085482f72ebc139684cbbaa64af490c1921d7
jaehyun <wogustkfkd11@gmail.com>
2023-02-19 21:24:35 +0900

    오타 수정

--- src/main/java/hello/exception/exception/BadRequestException.java
+++ src/main/java/hello/exception/exception/BadRequestException.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 /**
  * ResponseStatusExceptionResolver
  *
- * @ResponseStatus: 예외를 HTTP리 상태 코드로 예외를 변경해줌!
+ * @ResponseStatus: 예외를 HTTP 상태 코드로 예외를 변경해줌!
  */
 @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "error.bad")
 public class BadRequestException extends RuntimeException {
