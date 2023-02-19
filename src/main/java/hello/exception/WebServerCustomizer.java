1eb7baef976baeb1f183c709e29624961d8139e4
jaehyun <wogustkfkd11@gmail.com>
2023-02-19 11:39:01 +0900

    오류 페이지 커스텀마이징

--- src/main/java/hello/exception/WebServerCustomizer.java
+++ src/main/java/hello/exception/WebServerCustomizer.java
@@ -5,13 +5,17 @@ import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 
+/**
+ * 오류 페이지 커스텀마이징
+ * NOT_FOUND(404) 에러이면 "/error-page/404" 이동!
+ */
 //@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory) {
         ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND, "/error-page/404");
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
-        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500"); //2.에러를 전달받은 WAS는 "/error-page/500" 연결
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500"); //2.에러를 전달받은 WAS는 "/error-page/500" 연결 -> 다음: ErrorPageController
 
         factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
     }
