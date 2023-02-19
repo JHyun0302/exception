c2af5245648cb88c9b54c7d59e37eaf610c64716
jaehyun <wogustkfkd11@gmail.com>
2023-02-19 21:05:36 +0900

    예외 발생시 API 응답으로 사용하는 객체

--- src/main/java/hello/exception/exhandler/ErrorResult.java
+++ src/main/java/hello/exception/exhandler/ErrorResult.java
@@ -3,6 +3,9 @@ package hello.exception.exhandler;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * 예외 발생시 API 응답으로 사용하는 객체
+ */
 @Data
 @AllArgsConstructor
 public class ErrorResult {
