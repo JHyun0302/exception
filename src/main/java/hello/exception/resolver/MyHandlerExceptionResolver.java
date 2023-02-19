266b0634bd8560cf0e92c0baadcd9a79edc253b2
jaehyun <wogustkfkd11@gmail.com>
2023-02-19 21:15:25 +0900

    HandlerExceptionResolver 이용

--- src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
+++ src/main/java/hello/exception/resolver/MyHandlerExceptionResolver.java
@@ -8,6 +8,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * HandlerExceptionResolver - 예외 처리(정상 응답)
+ * IllegalArgumentException 발생시 SC_BAD_REQUEST(400) 에러로 바꿈
+ */
 @Slf4j
 public class MyHandlerExceptionResolver implements HandlerExceptionResolver {
 
